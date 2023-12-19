7555573a63a9fbf5e1e8ea0536ac24aed713234a
kinleyrabgay <fsd.rabgay@gmail.com>
2023-12-19 13:40:31 +0600

    first commit

---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,132 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+import 'components/animated_btn.dart';
+import 'components/custom_sign_in_dialog.dart';
+
+// Welcome back to Episode 2
+// Once user click on the btn
+// We want to show the dialog
+// Now come to the most intaresting part
+// How to show this loading error or success animation
+// I will show you on Episode 3
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  bool isSignInDialogShown = false;
+  late RiveAnimationController _btnAnimationColtroller;
+
+  @override
+  void initState() {
+    _btnAnimationColtroller = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset("assets/Backgrounds/Spline.png"),
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+            ),
+          ),
+          const RiveAnimation.asset("assets/RiveAssets/shapes.riv"),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: const SizedBox(),
+            ),
+          ),
+          AnimatedPositioned(
+            // It's time to add the animation
+            // Before that
+            top: isSignInDialogShown ? -50 : 0,
+            duration: const Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    const SizedBox(
+                      width: 260,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Learn design & code",
+                            style: TextStyle(
+                              fontSize: 60,
+                              fontFamily: "Poppins",
+                              height: 1.2,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Text(
+                            "Don’t skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools.",
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(flex: 2),
+                    AnimatedBtn(
+                      btnAnimationColtroller: _btnAnimationColtroller,
+                      press: () {
+                        _btnAnimationColtroller.isActive = true;
+                        Future.delayed(
+                          const Duration(milliseconds: 800),
+                          () {
+                            // We made it but
+                            // also need to set it false once the dialog close
+                            setState(() {
+                              isSignInDialogShown = true;
+                            });
+                            // Let's add the slide animation while dialog shows
+                            customSigninDialog(
+                              context,
+                              onCLosed: (_) {
+                                setState(() {
+                                  isSignInDialogShown = false;
+                                });
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Text(
+                        "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
